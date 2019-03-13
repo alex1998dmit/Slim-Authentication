5ddbf9752df7cf5b158c6ae74702f69b25f85489
alex1998dmit <alex1998dmit@gmail.com>
2019-03-13 15:35:30 +0300

    Add register controller

--- app/Controllers/Auth/AuthController.php
+++ app/Controllers/Auth/AuthController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controllers\Auth;
 
-// use App\Models\User;
+use App\Models\User;
 // use \Slim\Views\Twig as View; 
 use \App\Controllers\Controller as Controller;
 
@@ -16,7 +16,13 @@ class AuthController extends Controller
 
     public function postSignUp($request, $responce) 
     {
+        User::create([
+            'email' => $request->getParam('email'),
+            'name' => $request->getParam('name'),
+            'password' => password_hash($request->getParam('password'), PASSWORD_DEFAULT),
+        ]);
         
+        return $responce->withRedirect($this->router->pathFor('home'));
     }
 
 }
--- app/routes/index.php
+++ app/routes/index.php
@@ -2,7 +2,7 @@
 
 use App\Controllers\HomeController;
 
-$app->get('/hello', 'HomeController:index');
+$app->get('/hello', 'HomeController:index')->setName('home');
 
 $app->get('/auth/signup', 'AuthController:getSignUp')->setName('auth.signUp');
-$app->post('/auth/signup', 'AuthController:getSignUp');
+$app->post('/auth/signup', 'AuthController:postSignUp');
