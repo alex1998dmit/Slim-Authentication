ef170885cd42241a8cf0e15d67ffc72e31923cb7
alex1998dmit <alex1998dmit@gmail.com>
2019-03-13 14:15:19 +0300

    Add styles and scripts connection to twigs

--- app/routes/index.php
+++ app/routes/index.php
@@ -2,12 +2,6 @@
 
 use App\Controllers\HomeController;
 
-$app->get('/hello/{name}', function ($request, $response, $args) {
-    return $this->view->render($response, 'index.html', [
-        'name' => $args['name']
-    ]);
-})->setName('profile');
-
 $app->get('/hello', 'HomeController:index');
 
 // $app->get('/hello', 'HomeController:showAll');
